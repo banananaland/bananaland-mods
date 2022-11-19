5ce335a079ccb177271e305114da0971e69d0c46
taylorskalyo <taylorskalyo@users.noreply.github.com>
2022-11-19 11:58:24 -0500

    Implement @BuckarooBanzay's override fix
    https://github.com/banananaland/bananaland-mods/issues/21#issue-1343962362

--- extra_crafts/overrides.lua
+++ extra_crafts/overrides.lua
@@ -1,27 +1,3 @@
-minetest.register_node(":default:cobble", {
-	description = "Cobblestone",
-	tiles = {"default_cobble.png"},
-	is_ground_content = false,
-	groups = {cracky = 3, stone = 2},
-	sounds = default.node_sound_stone_defaults(),
-	stack_max = 9999,
-})
-
-minetest.register_node(":default:dirt", {
-	description = "Dirt",
-	tiles = {"default_dirt.png"},
-	groups = {crumbly = 3, soil = 1},
-	sounds = default.node_sound_dirt_defaults(),
-    stack_max = 9999,
-})
-
-
-minetest.register_node(":default:stone", {
-	description = "Stone",
-	tiles = {"default_stone.png"},
-	groups = {cracky = 3, stone = 1},
-	drop = 'default:cobble',
-	legacy_mineral = true,
-	sounds = default.node_sound_stone_defaults(),
-	stack_max = 9999,
-})
+minetest.override_item("default:stone", { stack_max = 9999 })
+minetest.override_item("default:cobble", { stack_max = 9999 })
+minetest.override_item("default:dirt", { stack_max = 9999 })
