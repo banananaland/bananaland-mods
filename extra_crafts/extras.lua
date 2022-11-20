7378c4ffc69a82585b7817ca33c74141c25af417
taylorskalyo <taylorskalyo@users.noreply.github.com>
2022-11-20 15:49:27 +0000

    Update extra_crafts and extra_stairsplus (#119)
    * Resolve conflicts between crafting recipes
    * Implement @BuckarooBanzay's override fix
    * Remove/move stairsplus registrations
    * Use mod.conf for extra_stairsplus
    * Add back cactusbrick micro blocks

---
+++ extra_crafts/extras.lua
@@ -0,0 +1,11 @@
+minetest.register_craft({
+  type = "shapeless",
+  output = "moreblocks:stone_tile 2",
+  recipe = { "xdecor:stone_tile" },
+})
+
+minetest.register_craft({
+  type = "shapeless",
+  output = "moreblocks:circle_stone_bricks",
+  recipe = { "xdecor:stone_rune" },
+})
---
+++ extra_crafts/overrides.lua
@@ -0,0 +1,3 @@
+minetest.override_item("default:stone", { stack_max = 9999 })
+minetest.override_item("default:cobble", { stack_max = 9999 })
+minetest.override_item("default:dirt", { stack_max = 9999 })
